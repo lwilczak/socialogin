3f0fe47ab0b8a8d0813b80b22ddd9d4689335534
lwilczak <lukasz@wilczak.net.pl>
2013-08-21 21:33:53 +0200

    clean application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Socialogin::Application.config.secret_key_base = 'edcff13aa32b907779b62033b6aa821cc3df471deaf1f2d369d6a6390e0c2518aa8b2fca769dad5d58980f4b30f86381c99160865e3314c338b6017aa5d8f493'
